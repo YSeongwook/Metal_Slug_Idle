ec92cbd369a304f1d32120d5549fcc155bf2c651
SeongWook <9892yyyssww@gmail.com>
2024-06-07 18:00:59 +0900

    [Feat] Firebase Realtime Database 채팅 제작

---
+++ Assets/02.Scripts/Data/Message.cs
@@ -0,0 +1,11 @@
+public class Message
+{
+    public string displayName;
+    public string text;
+
+    public Message(string displayName, string text)
+    {
+        this.displayName = displayName;
+        this.text = text;
+    }
+}
