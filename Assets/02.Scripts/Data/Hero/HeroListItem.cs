a7fe6833bcec7592fedde351a85890b14d9b1d71
SeongWook <9892yyyssww@gmail.com>
2024-06-25 18:51:38 +0900

    [Move] 폴더 변경

--- Assets/02.Scripts/Data/Hero/HeroListItem.cs
+++ Assets/02.Scripts/Data/Hero/HeroListItem.cs
@@ -53,7 +53,7 @@ public class HeroListItem : InfiniteScrollItem
             "근거리형" => typeSprites.Length > 0 ? typeSprites[0] : null,
             "원거리형" => typeSprites.Length > 1 ? typeSprites[1] : null,
             "방어형" => typeSprites.Length > 2 ? typeSprites[2] : null,
-            "지원형" => typeSprites.Length > 3 ? typeSprites[3] : null,
+            "만능형" => typeSprites.Length > 3 ? typeSprites[3] : null,
             _ => null
         };
     }
