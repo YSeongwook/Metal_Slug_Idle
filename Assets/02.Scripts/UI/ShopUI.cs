c2ef49e0292a32810165653b297b2a41cfb918f4
Seongwook <9892yyyssww@gmail.com>
2024-06-27 23:50:28 +0900

    [Feat] 상점 UI 제작 중

---
+++ Assets/02.Scripts/UI/ShopUI.cs
@@ -0,0 +1,87 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+// ShopUI 클래스는 SubUI의 기능을 확장하여 여러 버튼의 스프라이트를 관리합니다.
+public class ShopUI : SubUI
+{
+    // 버튼 UI 요소
+    [TabGroup("MainButton")] public Button button;
+    // 비활성화된 상태의 스프라이트
+    [TabGroup("MainButton")] public Sprite passiveSprite;
+    // 활성화된 상태의 스프라이트
+    [TabGroup("MainButton")] public Sprite activeSprite;
+
+    // 버튼의 이미지 컴포넌트
+    protected Image _buttonImage;
+
+    // 추가 버튼들
+    [TabGroup("SelectButton")] public Button selectHeroButton;
+    [TabGroup("SelectButton")] public Button selectPetButton;
+
+    // 버튼의 이미지 컴포넌트들
+    private Image _selectHeroImage;
+    private Image _selectPetImage;
+    
+    protected override void Awake()
+    {
+        // 부모 클래스의 Awake 메서드를 호출
+        base.Awake();
+        
+        _selectHeroImage = selectHeroButton.GetComponent<Image>();
+        _selectPetImage = selectPetButton.GetComponent<Image>();
+    }
+    
+    protected override void OnEnable()
+    {
+        // 부모 클래스의 OnEnable 메서드를 호출
+        base.OnEnable();
+        // 버튼 클릭 이벤트에 메서드를 등록
+        RegisterButtonListeners(true);
+    }
+    
+    protected override void OnDisable()
+    {
+        // 부모 클래스의 OnDisable 메서드를 호출
+        base.OnDisable();
+        // 버튼 클릭 이벤트에서 메서드를 제거
+        RegisterButtonListeners(false);
+    }
+
+    // 버튼 클릭 이벤트 등록/해제 메서드
+    private void RegisterButtonListeners(bool register)
+    {
+        if (register)
+        {
+            selectHeroButton.onClick.AddListener(OnSelectHeroButtonClicked);
+            selectPetButton.onClick.AddListener(OnSelectPetButtonClicked);
+        }
+        else
+        {
+            selectHeroButton.onClick.RemoveListener(OnSelectHeroButtonClicked);
+            selectPetButton.onClick.RemoveListener(OnSelectPetButtonClicked);
+        }
+    }
+
+    // selectHeroButton 클릭 이벤트 핸들러
+    private void OnSelectHeroButtonClicked()
+    {
+        SetImageAlpha(_selectHeroImage, 1f); // 알파값을 1로 설정 (255)
+        SetImageAlpha(_selectPetImage, 0f); // 알파값을 0으로 설정
+    }
+
+    // selectPetButton 클릭 이벤트 핸들러
+    private void OnSelectPetButtonClicked()
+    {
+        SetImageAlpha(_selectHeroImage, 0f); // 알파값을 0으로 설정
+        SetImageAlpha(_selectPetImage, 1f); // 알파값을 1로 설정 (255)
+    }
+
+    // 이미지의 알파 값을 설정하는 메서드
+    private void SetImageAlpha(Image image, float alpha)
+    {
+        Color color = image.color;
+        color.a = alpha;
+        image.color = color;
+    }
+}
--- Assets/02.Scripts/UI/SubUI.cs
+++ Assets/02.Scripts/UI/SubUI.cs
@@ -1,31 +1,44 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+// SubUI 클래스는 버튼의 스프라이트를 변경하는 기본 기능을 제공합니다.
 public class SubUI : MonoBehaviour
 {
+    // 버튼 UI 요소
     public Button button;
+    // 비활성화된 상태의 스프라이트
     public Sprite passiveSprite;
+    // 활성화된 상태의 스프라이트
     public Sprite activeSprite;
 
-    private Image _buttonImage;
+    // 버튼의 이미지 컴포넌트
+    protected Image _buttonImage;
 
-    private void Awake()
+    // 스크립트가 Awake될 때 호출됩니다.
+    protected virtual void Awake()
     {
+        // 버튼의 이미지 컴포넌트를 가져옵니다.
         _buttonImage = button.GetComponent<Image>();
     }
 
-    private void OnEnable()
+    // 오브젝트가 활성화될 때 호출됩니다.
+    protected virtual void OnEnable()
     {
+        // 버튼과 활성화 스프라이트가 null이 아닌 경우
         if (button != null && activeSprite != null)
         {
+            // 버튼의 스프라이트를 활성화 스프라이트로 변경합니다.
             _buttonImage.sprite = activeSprite;
         }
     }
 
-    private void OnDisable()
+    // 오브젝트가 비활성화될 때 호출됩니다.
+    protected virtual void OnDisable()
     {
+        // 버튼과 비활성화 스프라이트가 null이 아닌 경우
         if (button != null && passiveSprite != null)
         {
+            // 버튼의 스프라이트를 비활성화 스프라이트로 변경합니다.
             _buttonImage.sprite = passiveSprite;
         }
     }
