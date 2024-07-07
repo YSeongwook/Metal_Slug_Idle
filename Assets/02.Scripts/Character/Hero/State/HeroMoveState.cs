451f73694571f24c2c1085c1703c8d1cf13fe4f9
Seongwook <9892yyyssww@gmail.com>
2024-07-07 23:17:08 +0900

    [Fix] 타겟을 제대로 찾아 움직이나 약간 어색함

--- Assets/02.Scripts/Character/Hero/State/HeroMoveState.cs
+++ Assets/02.Scripts/Character/Hero/State/HeroMoveState.cs
@@ -3,60 +3,51 @@ using UnityEngine;
 public class HeroMoveState : IHeroState
 {
     private HeroController _hero;
-    private Vector3 _targetPosition;
-    private const float MaxDistanceFromLeader = 5f; // 리더로부터의 최대 거리
+    private Transform _target;
 
     public void EnterState(HeroController hero)
     {
         _hero = hero;
-        
-        // DebugLogger.Log("Enter MoveState");
+        SetTarget();
     }
 
     public void UpdateState()
     {
-        
+        if(_hero.CurrentTarget == null) _hero.TransitionToState(_hero.IdleState);
     }
 
     public void PhysicsUpdateState()
     {
         if (!_hero.IsAutoMode) return;
         
-        MoveToTarget();
+        MoveTowardsTarget();
     }
 
     public void ExitState() { }
 
-    public void SetTarget(Vector3 targetPosition)
+    private void SetTarget()
     {
-        _targetPosition = targetPosition;
+        _target = _hero.CurrentTarget;
     }
-
-    private void MoveToTarget()
+    
+    private void MoveTowardsTarget()
     {
-        if (_hero is FollowerController follower)
-        {
-            if (Vector3.Distance(_hero.transform.position, follower.leader.transform.position) > MaxDistanceFromLeader)
-            {
-                _targetPosition = follower.leader.transform.position;
-            }
-        }
+        if (_target == null) return;
 
-        Vector3 direction = (_targetPosition - _hero.transform.position).normalized;
-        Vector3 moveDirection = new Vector3(direction.x, 0, direction.z * _hero.heroStats.moveSpeed);
-        Vector3 newPosition = _hero.Rb.position + moveDirection * (_hero.heroStats.moveSpeed * Time.fixedDeltaTime);
-        newPosition.y = _hero.InitialY;
-        _hero.Rb.MovePosition(newPosition);
+        Vector3 direction = (_target.position - _hero.transform.position).normalized;
+        float distanceToMove = Vector3.Distance(_hero.transform.position, _target.position) - (_hero.heroStats.attackRange - 1f);
 
-        if (moveDirection != Vector3.zero)
+        if (distanceToMove > 0)
         {
-            _hero.HandleSpriteFlip(direction.x);
+            Vector3 moveDirection = direction * (_hero.heroStats.moveSpeed * Time.deltaTime);
+            Vector3 newPosition = _hero.Rb.position + moveDirection;
+
+            _hero.Rb.MovePosition(newPosition);
             _hero.Animator.SetFloat(_hero.SpeedParameter, moveDirection.magnitude);
         }
         else
         {
             _hero.Animator.SetFloat(_hero.SpeedParameter, 0);
-            _hero.TransitionToState(_hero.IdleState);
         }
     }
 }
